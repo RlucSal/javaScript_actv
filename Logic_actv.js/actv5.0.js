ecf83a2e402e3c1b7cc7349bc07f4611e6c765c7
Rosa Lucena <rluc.sal@gmail.com>
2025-02-17 20:32:11 +0000

    added js actv

---
+++ Logic_actv.js/actv5.0.js
@@ -0,0 +1,16 @@
+/*Write a function findMax that returns the largest number in a given array.*/
+
+let numbers = [1,24,56,43,78,98,56,112,3,10];
+
+let maxNumber = Math.max (...numbers);
+
+console.log(maxNumber);
+
+
+
+
+
+
+
+
+
